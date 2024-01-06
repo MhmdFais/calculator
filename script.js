8a0e4976db3db7fc9a7640fecd2623759ac95d64
Muhammed <117585798+MhmdFais@users.noreply.github.com>
2024-01-07 01:32:28 +0530

    further styling and basic js to display the inputs

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
-let userSelected = document.getElementById(buttonPress).value;
+//BUTTONS VARIABLES
+let deleteOneDigit = document.querySelector('#delete-digit');
+let deleteAll = document.querySelector('#delete-all');
+let operationsButton = document.querySelectorAll('#op');
+let equal = document.querySelector('#long-button');
+let numbersOnCal = document.querySelectorAll('#num');
+let currentOperationDisply = document.querySelector('#current-operation');
+let previousOperationDisplay = document.querySelector('#previous-operation');
 
-console.log(userSelected);
+let currentNumberString = '';
+let firstOperant = null;
+let secondOperant = null;
+
+//PRESSED NUMBERS 
+numbersOnCal.forEach((digit) => {
+    digit.addEventListener('click', (event) =>{
+        currentNumberString += event.target.innerHTML;
+        displayNumber(currentNumberString);
+    });
+});
+
+//DISPLAY THE SELECTED NUMBERS 
+function displayNumber(number){
+    previousOperationDisplay.textContent = number;
+}
+
+//AN OPERATION IS PRESSED
+operationsButton.forEach((opra) => {
+    opra.addEventListener('click', () => {
+        firstOperant = parseFloat(currentNumberString);
+        currentNumberString += (' '+opra.innerHTML+' ');
+        displayNumber(currentNumberString);
+    })
+});
+
+
+
